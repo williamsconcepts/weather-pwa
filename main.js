c25c4ebee085972f2dbd8c934f18a366516289e3
williamsconcepts <chidiekwebelam@gmail.com>
2020-09-07 00:31:50 +0100

    today

--- main.js
+++ main.js
@@ -53,3 +53,16 @@ function dateBuilder (d) {
   return `${day} ${date} ${month} ${year}`;
 }
 
+
+// SERVICE WORKER
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('./service-worker.js', { scope: './'})
+  .then(function(registration) {
+    console.log("service worker registered", registration);
+  })
+  .catch(function(err) {
+    console.log("service worker failed registration", err);
+  })
+}
